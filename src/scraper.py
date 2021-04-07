7447c5c77046a37f5141d8b0362beb06d44c7716
Pato Bonilla <gpbonillas@gmail.com>
2021-04-07 17:51:09 -0500

    Get data from book list (first page)

--- src/scraper.py
+++ src/scraper.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 baseurl = "https://www.springer.com/la"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
 
-k = requests.get(baseurl).text
+# k = requests.get(baseurl).text
 # parse to html
-soup=BeautifulSoup(k,'html.parser')
+# soup=BeautifulSoup(k,'html.parser')
 # get all disciplines
-booklist = soup.find_all("ul",{"class":"cms-col cms-link-list"})
+# booklist = soup.find_all("ul",{"class":"cms-col cms-link-list"})
 
 # print(booklist)
 
@@ -19,6 +19,23 @@ k1 = requests.get(first_url).text.encode('utf8').decode('ascii', 'ignore')
 # parse to html
 soup_fu=BeautifulSoup(k1,'html.parser')
 # get all disciplines
-first_booklist = soup_fu.find_all("div",{"class":"result-type-book"})
+first_booklist_page = soup_fu.find_all("div",{"class":"result-type-book"})
 
-print(first_booklist)
+books = []
+
+
+for book in first_booklist_page:
+    # name = book.find("a",{"class":"product-card"}).get('href')                 
+    # productlinks.append(baseurl + link)
+    books.append({  
+        'bookname': book.h4.a.string, 
+        'authors': book.find('p', attrs={'class':'meta contributors book-contributors'}).get_text(), 
+        'price': book.find('span', attrs={'class':'price'})
+        })
+    # print(book.h4.a.string)
+
+
+for book in books:
+    print(book)
+
+print(first_booklist_page)
